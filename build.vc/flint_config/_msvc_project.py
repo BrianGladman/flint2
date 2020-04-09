e96e4472e840111057e75f5dd80e7b6cf080b8e9
Brian Gladman <brg@gladman.plus.com>
2020-04-09 09:01:27 +0100

    add Visual Studio 2019 build files and build generator files

---
+++ build.vc/flint_config/_msvc_project.py
@@ -0,0 +1,291 @@
+﻿# generate vcxproj file
+
+from os.path import normpath, join, split, relpath
+from os import access, F_OK
+from enum import IntEnum
+
+class Project_Type(IntEnum):
+  APP = 0
+  LIB = 1
+  DLL = 2
+
+app_ext = ('.exe', '.lib', '.dll')
+app_str = ('Application', 'StaticLibrary', 'DynamicLibrary')
+
+def vcx_proj_cfg(plat, outf):
+
+  f1 = r'''  <ItemGroup Label="ProjectConfigurations">
+'''
+  f2 = r'''    <ProjectConfiguration Include="{1:s}|{0:s}">
+      <Configuration>{1:s}</Configuration>
+      <Platform>{0:s}</Platform>
+    </ProjectConfiguration>
+'''
+  f3 = r'''  </ItemGroup>
+'''
+  outf.write(f1)
+  for pl in plat:
+    for conf in ('Release', 'Debug'):
+      outf.write(f2.format(pl, conf))
+  outf.write(f3)
+
+def vcx_globals(name, guid, vs_info, outf):
+
+  f1 = r'''  <PropertyGroup Label="Globals">
+    <RootNamespace>{0:s}</RootNamespace>
+    <Keyword>Win32Proj</Keyword>
+    <ProjectGuid>{1:s}</ProjectGuid>
+    <WindowsTargetPlatformVersion>{2:s}</WindowsTargetPlatformVersion>
+  </PropertyGroup>
+'''
+  outf.write(f1.format(name, guid, vs_info['windows_sdk']))
+
+def vcx_default_cpp_props(outf):
+
+  f1 = r'''  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />
+'''
+  outf.write(f1)
+
+def vcx_library_type(plat, proj_type, vs_info, outf):
+
+  f1 = r'''  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'" Label="Configuration">
+    <ConfigurationType>{2:s}</ConfigurationType>
+    <UseDebugLibraries>{3:s}</UseDebugLibraries>
+    <PlatformToolset>v{4:s}</PlatformToolset>
+  </PropertyGroup>
+'''
+  for pl in plat:
+    for conf, bool in (('Release', 'false'), ('Debug', 'true')):
+      outf.write(f1.format(pl, conf, app_str[proj_type], bool, vs_info['platform_toolset']))
+
+def vcx_cpp_props(outf):
+
+  f1 = r'''  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
+'''
+  outf.write(f1)
+
+def vcx_extensions(outf, dir):
+
+  f1 = r'''  <ImportGroup Label="ExtensionSettings">
+    <Import Project="{0:s}vsyasm.props" />
+  </ImportGroup>
+'''
+  outf.write(f1.format(dir))
+
+def vcx_user_props(plat, proj_type, outf):
+
+  f1 = r'''  <ImportGroup Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'" Label="PropertySheets">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" />
+    <Import Project="..\..\{4:s}build.vc\flint_{2:s}_{3:s}.props" />
+  </ImportGroup>
+'''
+  depth = '..\\' if proj_type == Project_Type.APP else ''
+  for pl in plat:
+    for conf in ('Release', 'Debug'):
+      outf.write(f1.format(pl, conf, conf.lower(), app_ext[proj_type][1:], depth))
+
+def vcx_target_name_and_dirs(proj_name, proj_dir, plat, outf):
+
+  f1 = r'''  <PropertyGroup>
+    <_ProjectFileVersion>10.0.21006.1</_ProjectFileVersion>
+'''
+  f2 = r'''    <TargetName Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'">{2:s}</TargetName>
+    <IntDir Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'">$(Platform)\$(Configuration)\</IntDir>
+    <OutDir Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'">$(SolutionDir){3:s}$(Platform)\$(Configuration)\</OutDir>
+    <LinkIncremental>false</LinkIncremental>
+'''
+  f3 = r'''  </PropertyGroup>
+'''
+  if not proj_dir:
+    proj_dir = ''
+  elif not (proj_dir.endswith('\\') or proj_dir.endswith('/')):
+    proj_dir += '\\'
+
+  outf.write(f1)
+  for pl in plat:
+    for conf in ('Release', 'Debug'):
+      outf.write(f2.format(pl, conf, proj_name, proj_dir))
+  outf.write(f3)
+
+def compiler_options(plat, proj_type, is_debug, inc_dirs, flib_type, outf):
+
+  f1 = r'''    <ClCompile>
+    <Optimization>{0:s}</Optimization>
+    <IntrinsicFunctions>true</IntrinsicFunctions>
+    <AdditionalIncludeDirectories>{1:s}</AdditionalIncludeDirectories>
+    <PreprocessorDefinitions>{2:s}%(PreprocessorDefinitions)</PreprocessorDefinitions>
+    <RuntimeLibrary>MultiThreaded{3:s}</RuntimeLibrary>
+    <ProgramDataBaseFileName>$(TargetDir)$(TargetName).pdb</ProgramDataBaseFileName>
+    <DebugInformationFormat>{4:s}</DebugInformationFormat>
+    </ClCompile>
+'''
+
+  if proj_type == Project_Type.APP:
+    s1 = 'DEBUG;WIN32;_CONSOLE;PTW32_STATIC_LIB;'
+    s2 = ''
+  if proj_type == Project_Type.DLL:
+    s1 = 'DEBUG;WIN32;HAVE_CONFIG_H;MSC_BUILD_DLL;PTW32_BUILD;'
+    s2 = 'DLL'
+  elif proj_type == Project_Type.LIB:
+    s1 = 'DEBUG;WIN32;_LIB;HAVE_CONFIG_H;PTW32_STATIC_LIB;'
+    s2 = ''
+  else:
+    pass
+  if flib_type == 'single':
+    s1 += 'FLINT_REENTRANT=0;HAVE_TLS=1;'
+  elif flib_type == 'reentrant':
+    s1 += 'FLINT_REENTRANT=1;'
+  else:
+    pass
+  if plat == 'x64':
+    s1 = s1 + '_WIN64;'
+  if is_debug:
+    opt, defines, crt, dbf = 'Disabled', '_' + s1, 'Debug' + s2, 'ProgramDatabase'
+  else:
+    opt, defines, crt, dbf = 'Full', 'N' + s1, s2, 'None'
+  outf.write(f1.format(opt, inc_dirs, defines, crt, dbf))
+
+def linker_options(name, link_libs, proj_type, debug_info, outf):
+
+  f1 = r'''    <Link>
+'''
+  f2 = r'''      <GenerateDebugInformation>true</GenerateDebugInformation>
+'''
+  f3 = r'''      <LargeAddressAware>true</LargeAddressAware>
+      <AdditionalDependencies>{}%(AdditionalDependencies)</AdditionalDependencies>
+'''
+  f4 = '''      <ModuleDefinitionFile>$(SolutionDir){}.def</ModuleDefinitionFile>
+'''
+  f5 = '''    </Link>
+'''
+  outf.write(f1)
+  if debug_info:
+    outf.write(f2)
+  outf.write(f3.format(link_libs))
+  # no longer needed as we are using the declspec approach
+  #  if proj_type == Project_Type.DLL:
+  #    outf.write(f4.format(name))
+  outf.write(f5)
+
+def vcx_pre_build(outf):
+
+  f1 = r'''    <PreBuildEvent>
+        <Command>..\..\build.vc\out_copy_rename.bat ..\..\build.vc\cpimport.h ..\..\qadic\ cpimport.h
+        </Command>
+    </PreBuildEvent>
+'''
+
+  outf.write(f1)
+
+def vcx_post_build(proj_type, msvc_ver, outf):
+
+  f1 = r'''
+  <PostBuildEvent>
+      <Command>..\..\build.vc\postbuild $(IntDir) {} {}
+      </Command>
+  </PostBuildEvent>
+'''
+
+  outf.write(f1.format((app_ext[proj_type][1:]).upper(), msvc_ver))
+
+def vcx_tool_options(name, plat, proj_type, prebuild, postbuild, inc_dirs, link_libs, debug_info, msvc_ver, flib_type, outf):
+
+  f1 = r'''  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='{1:s}|{0:s}'">
+'''
+  f2 = r'''  </ItemDefinitionGroup>
+'''
+  for pl in plat:
+    for is_debug in (False, True):
+      outf.write(f1.format(pl, 'Debug' if is_debug else 'Release'))
+      if prebuild:
+        vcx_pre_build(outf)
+      compiler_options(pl, proj_type, is_debug, inc_dirs, flib_type, outf)
+      if proj_type != Project_Type.LIB:
+        linker_options(name, link_libs, proj_type, debug_info, outf)
+      if postbuild:
+        vcx_post_build(proj_type, msvc_ver, outf)
+      outf.write(f2)
+
+def vcx_external_props(outf):
+
+  f1 = r'''  <ImportGroup>
+    <Import Condition="exists('$(MPIR_Props_External)')" Project="$(MPIR_Props_External)" />
+  </ImportGroup>
+'''
+  outf.write(f1)
+
+def vcx_hdr_items(hdr_list, relp, outf):
+
+  f1 = r'''  <ItemGroup>
+'''
+  f2 = r'''    <ClInclude Include="{}{}" />
+'''
+  f3 = r'''  </ItemGroup>
+'''
+  outf.write(f1)
+  for i in hdr_list:
+    outf.write(f2.format(relp, i))
+  outf.write(f3)
+
+def vcx_c_items(cf_list, plat, relp, flib_type, outf):
+
+  f1 = r'''  <ItemGroup>
+'''
+  f2 = r'''    <ClCompile Include="{}{}" />
+'''
+  f3 = r'''    <ClCompile Include="{}{}">
+'''
+  f4 = r'''        <ObjectFileName Condition="'$(Configuration)|$(Platform)'=='{0:s}|{1:s}'">$(IntDir){2:s}\</ObjectFileName>
+'''
+  f5 = r'''      <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='{0:s}|{1:s}'">true</ExcludedFromBuild>
+'''
+  f6 = r'''    </ClCompile>
+'''
+  f7 = r'''  </ItemGroup>
+'''
+
+  outf.write(f1)
+  for nxd in cf_list:
+    if nxd[0] == 'link':
+      ts = split(nxd[1])[1]
+      ts = ts.replace('fmpz_', '')
+      ts = ts.replace('.c', '')
+      if ts != flib_type:
+        outf.write(f3.format(relp, nxd[1]))
+        for cf in ('Release', 'Debug'):
+          for pl in plat:
+            outf.write(f5.format(cf, pl))
+        outf.write(f6)
+        continue
+    outf.write(f2.format(relp, nxd[1]))
+  outf.write(f7)
+
+def gen_vcxproj(path, root_dir, proj_name, guid, plat, proj_type, flib_type, prebuild, postbuild, debug_info, hf_list, cf_list, inc_dirs, link_libs, vs_info):
+
+  f1 = r'''<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" ToolsVersion="{}" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+'''
+  f2 = r'''  <PropertyGroup Label="UserMacros" />
+'''
+  f3 = r'''  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
+</Project>
+'''
+
+  relp = split(relpath(root_dir, path))[0] + '\\'
+  project_dir = ''
+  with open(path, 'w') as outf:
+    outf.write(f1.format(vs_info['vcx_tool']))
+    vcx_proj_cfg(plat, outf)
+    vcx_globals(proj_name, guid, vs_info, outf)
+    vcx_default_cpp_props(outf)
+    vcx_library_type(plat, proj_type, vs_info, outf)
+    vcx_cpp_props(outf)
+    vcx_user_props(plat, proj_type, outf)
+    outf.write(f2)
+    vcx_target_name_and_dirs(proj_name, project_dir, plat, outf)
+    vcx_tool_options(proj_name, plat, proj_type, prebuild, postbuild, inc_dirs, link_libs, debug_info, vs_info['msvc'], flib_type, outf)
+    vcx_external_props(outf)
+    if hf_list:
+      vcx_hdr_items(hf_list, relp, outf)
+    vcx_c_items(cf_list, plat, relp, flib_type, outf)
+    outf.write(f3)
---
+++ build.vc/flint_config/flint_config.py
@@ -0,0 +1,341 @@
+﻿'''
+Set up Visual Sudio to build a specified MPIR configuration
+
+Copyright (C) 2011, 2012, 2013, 2014 Brian Gladman
+'''
+
+from operator import itemgetter
+from os import scandir, walk, unlink, makedirs, remove
+from os.path import join, split, splitext, isdir, exists
+from os.path import dirname, abspath, relpath, realpath, normpath
+from copy import deepcopy
+from sys import argv, exit, path
+from filecmp import cmp
+from shutil import copy
+from re import compile, search
+from collections import defaultdict
+from uuid import uuid4
+from time import sleep
+
+from _msvc_filters import gen_filter
+from _msvc_project import Project_Type, gen_vcxproj
+from _msvc_solution import msvc_solution
+
+vs_version = 19
+if len(argv) > 1:
+  vs_version = int(argv[1])
+
+project_name = 'flint'
+script_dir = dirname(realpath(__file__))
+build_dir_name = 'build.vs{0:d}'.format(vs_version)
+build_root_dir = abspath(join(script_dir, '..\\..\\' + build_dir_name))
+build_aux_dir = abspath(join(script_dir, '..\\..\\' + 'build.vc'))
+path.append(build_root_dir)
+from version_info import vs_info
+
+# add user choice (duplicate in _msvc_project.py)
+flib_type = 'single' # ('gc', 'reentrant', 'single')
+
+# The path to flint, solution and project directories
+flint_dir = abspath(join(script_dir, '../../'))
+solution_dir = abspath(join(flint_dir, build_dir_name))
+
+# for script debugging
+debug = False
+# what to build
+build_lib = True
+build_dll = True
+build_tests = False
+build_profiles = False
+
+# copy from file ipath to file opath but avoid copying if
+# opath exists and is the same as ipath (this is to avoid
+# triggering an unecessary rebuild).
+
+def write_f(ipath, opath):
+  if exists(ipath) and not isdir(ipath):
+    if exists(opath):
+      if isdir(opath) or cmp(ipath, opath):
+        return
+    copy(ipath, opath)
+
+ignore_dirs = ( '.git', '.vs', 'doc', 'examples', 'lib', 'exe', 'dll', 'win_hdrs')
+req_extns = ( '.h', '.c', '.cc', '.cpp' )
+
+def find_src(path):
+
+  c, h, cx, hx, t, tx, p = [], [], [], [], [], [], []
+  for root, dirs, files in walk(path):
+    if 'template' in root:
+      continue
+    _, _t = split(root)
+    if _t in ignore_dirs:
+      print('removed : ', _t)
+      continue
+    if build_dir_name in root:
+      for di in list(dirs):
+        dirs.remove(di)
+    for di in list(dirs):
+      if di in ignore_dirs or search(r'\.v(c|s)\d+', di):
+        dirs.remove(di)
+      if 'template' in di:
+        dirs.remove(di)
+    relp = relpath(root, flint_dir)
+    if relp == '.':
+      relp = ''
+    for f in files:
+      if 'template' in f:
+        continue
+      n, x = splitext(f)
+      if x not in req_extns:
+        continue
+      pth, leaf = split(root)
+      fp = join(relp, f)
+      if leaf == 'tune':
+        continue
+      if leaf == 'test':
+        p2, l2 = split(pth)
+        l2 = '' if l2 == 'flint2' else l2
+        if 'flintxx' in pth:
+          tx += [(l2, fp)]
+        else:
+          t += [(l2, fp)]
+      elif leaf == 'profile':
+        p2, l2 = split(pth)
+        l2 = '' if l2 == 'flint2' else l2
+        p += [(l2, fp)]
+      elif leaf == 'flintxx':
+        cx += [fp]
+      elif x == '.c':
+        c += [(leaf, fp)]
+      elif x == '.h':
+        if n.endswith('xx'):
+          hx += [fp]
+        else:
+          h += [fp]
+  for x in (c, h, cx, hx, t, tx, p):
+    x.sort()
+  return (c, h, cx, hx, t, tx, p)
+
+s_sym = r'([_a-zA-Z][_a-zA-Z0-9]*)'
+dec = '^(?:' + s_sym + '\s+)?' + s_sym + '\('
+re_dec = compile(dec)
+re_end = compile('\s*\);\s*$')
+
+# crude parser to detect function declarations in
+# FLINT header files (no longer needed but left in
+# place in case this changes).
+
+def strip_static_inline(lines, pos, lth):
+  p0 = pos
+  m = re_end.search(lines[pos])
+  pos += 1
+  if m:
+    return pos
+  level = 0
+  while pos < lth:
+    line = (lines[pos].replace('\n', ' ')).strip()
+    m = re_end.search(lines[pos])
+    if not level and (m or not line or pos > p0 + 5) or pos >= lth:
+      return pos + 1
+    level += line.count('{') - line.count('}')
+    pos += 1
+  return pos
+
+def parse_hdrs(h):
+  d = defaultdict(list)
+  for hf in h:
+    lines = open(join(flint_dir, hf), 'r').readlines()
+    pos, n_lines = 0, len(lines)
+    line_buf = ''
+    in_dec = 0
+    while pos < n_lines:
+      if in_dec == 0:
+        line_buf = ''
+      line = (lines[pos].replace('\n', ' ')).strip()
+      if not line:
+        pos += 1
+        continue
+      if line.startswith('#define'):
+        while line.endswith('\\') and pos < n_lines:
+          pos += 1
+          line = (lines[pos].replace('\n', ' ')).strip()
+        pos += 1
+        continue
+      if 'INLINE' in line.upper():
+        pos = strip_static_inline(lines, pos, n_lines)
+        in_dec = 0
+        continue
+      if not in_dec:
+        m = re_dec.search(line)
+        if m:
+          line_buf += line + ' '
+          mm = re_end.search(line)
+          if mm:
+            pos += 1
+            print(m.group(2))
+            d[hf] += [(pos, m.group(2), line_buf)]
+            in_dec = 0
+            continue
+          in_dec += 1
+      else:
+        line_buf += line
+        mm = re_end.search(line)
+        if mm:
+          if in_dec < 5:
+            d[hf] += [(pos - in_dec + 1, m.group(2), line_buf)]
+          in_dec = 0
+        else:
+          in_dec += 1
+      pos += 1
+  return d
+
+def write_hdrs(h):
+  d = parse_hdrs(h)
+  hdr_dir = join(flint_dir, 'win_hdrs')
+  if not exists(hdr_dir):
+    makedirs(hdr_dir)
+
+  for hf in sorted(d.keys()):
+    out_name = hf.replace('build.vc14\\', '')
+    inf = open(join(flint_dir, hf), 'r')
+    outf = open(join(flint_dir, join(hdr_dir, out_name)), 'w')
+    lines = inf.readlines()
+    for n, _p, _d in d[hf]:
+      lines[n - 1] = 'FLINT_DLL ' + lines[n - 1]
+    outf.writelines(lines)
+    inf.close()
+    outf.close()
+
+def write_def_file(name, h):
+  d = parse_hdrs(h)
+  lines = ['LIBRARY ' + name + '\n', 'EXPORTS' + '\n']
+  for hf in sorted(d.keys()):
+    for _n, sym, _d in d[hf]:
+      lines += ['    ' + sym + '\n']
+  with open(join(solution_dir, name + '.def'), 'w') as outf:
+    outf.writelines(lines)
+
+# end of parser code
+
+
+# add project files to the solution
+
+c, h, cx, hx, t, tx, p = find_src(flint_dir)
+
+# write_hdrs(h)
+
+if not debug:
+  with open('..\\..\\qadic\\CPimport.txt', 'r') as fin:
+    with open('tmp.h', 'w') as fout:
+      while True:
+        l = fin.readline()
+        if not l:
+          break
+        l = l.replace(' ', ',')
+        l = l.replace('\n', ',\n')
+        fout.writelines([l])
+  write_f('tmp.h', join(build_aux_dir, 'cpimport.h'))
+  remove('tmp.h')
+
+  fn = join(flint_dir, 'fmpz-conversions-{}.in'.format(flib_type))
+  copy(fn , join(flint_dir, 'fmpz-conversions.h'))
+  # fn = join(flint_dir, 'fft_tuning32.in')
+  fn = join(flint_dir, 'fft_tuning64.in')
+  copy(fn , join(flint_dir, 'fft_tuning.h'))
+  solution_name = project_name + '.sln'
+  # write_hdrs(h)
+
+# input any existing projects in the solution (*.sln) file
+solc = msvc_solution(abspath(join(solution_dir, solution_name)))
+
+if build_lib:
+  # set up LIB build
+  proj_name = 'lib_flint'
+  vcx_name = 'lib_flint'
+  vcx_path = abspath(join(solution_dir, vcx_name, vcx_name + '.vcxproj'))
+  gen_filter(vcx_path + '.filters', flint_dir, h, c, [], '12.0')
+  guid = solc.get_project_guid(vcx_name, vcx_path)
+  mode = ('Win32', 'x64')
+  inc_dirs = r'..\..\;..\..\build.vc;..\..\..\mpir\lib\$(IntDir);..\..\..\mpfr\lib\$(IntDir);..\..\..\pthreads\lib\$(IntDir)'
+  link_libs = r'..\..\..\mpir\lib\$(IntDir)mpir.lib;..\..\..\mpfr\lib\$(IntDir)mpfr.lib;..\..\..\pthreads\lib\$(IntDir)pthreads.lib'
+  gen_vcxproj(vcx_path, flint_dir, proj_name, guid, mode, Project_Type.LIB, flib_type, True, True, True, h, c, inc_dirs, link_libs, vs_info)
+  solc.add_project('', vcx_name, vcx_path, guid, mode)
+
+if build_dll:
+  # set up DLL build
+
+  # no longer needed
+  # write_def_file('dll_flint', h)
+
+  proj_name = 'dll_flint'
+  vcx_name = 'dll_flint'
+  vcx_path = abspath(join(solution_dir, vcx_name, vcx_name + '.vcxproj'))
+  gen_filter(vcx_path + '.filters', flint_dir, h, c, [], '12.0')
+  guid = solc.get_project_guid(vcx_name, vcx_path)
+  mode = ('Win32', 'x64')
+  inc_dirs = r'..\..\;..\..\build.vc;..\..\..\mpir\dll\$(IntDir);..\..\..\mpfr\dll\$(IntDir);..\..\..\pthreads\dll\$(IntDir);'
+  link_libs = r'..\..\..\mpir\dll\$(IntDir)mpir.lib;..\..\..\mpfr\dll\$(IntDir)mpfr.lib;..\..\..\pthreads\dll\$(IntDir)pthreads.lib;'
+  gen_vcxproj(vcx_path, flint_dir, proj_name, guid, mode, Project_Type.DLL, flib_type, True, True, True, h, c, inc_dirs, link_libs, vs_info)
+  solc.add_project('', vcx_name, vcx_path, guid, mode)
+
+solc.write_solution(vs_info)
+
+def gen_test(sol, solf, test_name, directory, proj_dir, name, c_file):
+  # set up LIB build
+  proj_name = vcx_name = test_name[2:]
+  vcx_path = abspath(join(solution_dir, directory, name + '_' + vcx_name, vcx_name + '.vcxproj'))
+  gen_filter(vcx_path + '.filters', solution_dir, [], [('', c_file)], [], '12.0')
+  guid = sol.get_project_guid(vcx_name, vcx_path)
+  mode = ('Win32', 'x64')
+  inc_dirs = r'..\..\;..\..\..\;..\..\..\..\mpir\lib\$(IntDir);..\..\..\..\mpfr\lib\$(IntDir);..\..\..\..\pthreads\lib\$(IntDir);'
+  link_libs = r'..\..\..\lib\$(IntDir)lib_flint.lib;..\..\..\..\mpir\lib\$(IntDir)mpir.lib;..\..\..\..\mpfr\lib\$(IntDir)mpfr.lib;..\..\..\..\pthreads\lib\$(IntDir)pthreads.lib;'
+  gen_vcxproj(vcx_path, flint_dir, test_name, guid, mode, Project_Type.APP, flib_type, False, False, False, [], [('', c_file)], inc_dirs, link_libs, vs_info)
+  sol.add_project(solf, vcx_name, vcx_path, guid)
+
+def gen_tests(sln_name, directory, proj_dir, c_files):
+  sn = sln_name[:sln_name.rfind('.')]
+  for cnt, (name, fpath) in enumerate(c_files):
+    soln = sn + str(cnt // 100 + 1) + '.sln'
+    if cnt % 100 == 0:
+      print(soln)
+      solc = msvc_solution(abspath(join(solution_dir, directory, soln)))
+    print('  ', cnt, name, fpath)
+    _, t = split(fpath)
+    if t[:2] not in ('t-', 'p-'):
+      continue
+    p_name = t.replace('.c', '')
+    gen_test(solc, name, p_name, directory, proj_dir, name, fpath)
+    if cnt % 100 == 99:
+      solc.write_solution(vs_info)
+  if cnt % 100:
+    solc.write_solution(vs_info)
+
+if build_tests:
+  gen_tests('flint-tests.sln', 'flint-tests', 'tests', t)
+
+if build_profiles:
+  gen_tests('flint-profiles.sln', 'flint-profiles', 'profiles', p)
+
+if debug:
+  print('\nC files')
+  for d in c:
+    print('  ', d)
+  print('\nC header files')
+  for d in h:
+    print('  ', d)
+  print('\nC++ files')
+  for d in cx:
+    print('  ', d)
+  print('\nC++ header files')
+  for d in hx:
+    print('  ', d)
+  print('\nC Test files')
+  for d in t:
+    print('  ', d)
+  print('\nC++ Test files')
+  for d in tx:
+    print('  ', d)
+  print('\nProfile files')
+  for d in p:
+    print('  ', d)
